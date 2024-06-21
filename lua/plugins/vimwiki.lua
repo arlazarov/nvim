f025eca7edefab973615f5f346e2a6518a29eb20
arlazarov <mrarlazarov@gmail.com>
2024-06-21 18:15:34 -0400

    Initial Commit

--- lua/plugins/vimwiki.lua
+++ lua/plugins/vimwiki.lua
@@ -1,16 +1,23 @@
-return{
+return {
 	"vimwiki/vimwiki",
 	init = function()
 		vim.g.vimwiki_list = {
-				{
-				path = '~/vimwiki',
-				syntax = 'default',
-				ext = '.wiki',
-				},
+			{
+				path = "~/.config/nvim/vimwiki/",
+				syntax = "markdown",
+				ext = ".md",
+			},
 		}
-		vim.g.vimwiki_map_prefix = '<leader>w'
+		vim.g.vimwiki_auto_chdir = 1
+		vim.g.vimwiki_date_format = "%Y-%m-%d"
+		vim.g.vimwiki_auto_toc = 1
+		vim.g.vimwiki_headers_level = 2
+		vim.g.vimwiki_autocomplete_syntax = 1
+		vim.g.vimwiki_markdown_link_ext = 1
+
+		vim.g.vimwiki_map_prefix = "<leader>w"
 		vim.g.vimwiki_camel_case = 0
 		vim.g.vimwiki_table_auto_fmt = 1
 		vim.g.vimwiki_use_calendar = 1
-    end,
+	end,
 }
